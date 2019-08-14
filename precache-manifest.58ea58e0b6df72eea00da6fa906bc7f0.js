276e8da5136a8a6c860b485af63e49af8948e999
salvadorsalgado1 <salvadorsalgado5694@gmail.com>
2019-08-13 22:46:21 -0700

    Updates

---
+++ precache-manifest.58ea58e0b6df72eea00da6fa906bc7f0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2421061fad479f09b445f77961cf0d21",
+    "url": "salvadorsalgado1.github.io/BankApp/index.html"
+  },
+  {
+    "revision": "6cc6de3a3af8b0a64e6e",
+    "url": "salvadorsalgado1.github.io/BankApp/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "8ad6d48c732d732a3211",
+    "url": "salvadorsalgado1.github.io/BankApp/static/css/main.a5355045.chunk.css"
+  },
+  {
+    "revision": "6cc6de3a3af8b0a64e6e",
+    "url": "salvadorsalgado1.github.io/BankApp/static/js/2.7bd41348.chunk.js"
+  },
+  {
+    "revision": "8ad6d48c732d732a3211",
+    "url": "salvadorsalgado1.github.io/BankApp/static/js/main.302f2db7.chunk.js"
+  },
+  {
+    "revision": "4602ed833c93584898aa",
+    "url": "salvadorsalgado1.github.io/BankApp/static/js/runtime~main.fccea866.js"
+  },
+  {
+    "revision": "c81b225e1d0093054aff3a3b4c7ee43d",
+    "url": "salvadorsalgado1.github.io/BankApp/static/media/Landscape.c81b225e.jpg"
+  },
+  {
+    "revision": "fc49ed0102e9c8d3d86737cbaa32e451",
+    "url": "salvadorsalgado1.github.io/BankApp/static/media/NightCity.fc49ed01.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "salvadorsalgado1.github.io/BankApp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b6681619dcc616623083589aa826d2be",
+    "url": "salvadorsalgado1.github.io/BankApp/static/media/profilepicture.b6681619.png"
+  }
+]);
